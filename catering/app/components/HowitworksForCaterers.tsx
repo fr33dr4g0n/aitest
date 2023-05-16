57293de34679badbd59ec08c3f4bfe53200a9523
fr33dr4g0n <henrikharju22@hotmail.com>
2023-05-16 17:15:47 +0000

    fixed commas

--- catering/app/components/HowitworksForCaterers.tsx
+++ catering/app/components/HowitworksForCaterers.tsx
@@ -32,7 +32,7 @@ export default function HowitworksCaterer() {
           <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
           How it works          </p>
           <p className="mt-6 text-lg leading-8 text-gray-600">
-          Eliminate event planning stress – simply fill out one form and watch multiple caterers offer their top menus, all seeking your taste buds' delight.          </p>
+          Eliminate event planning stress – simply fill out one form and watch multiple caterers offer their top menus, all seeking your taste buds&apos; delight.          </p>
         </div>
         <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
           <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
